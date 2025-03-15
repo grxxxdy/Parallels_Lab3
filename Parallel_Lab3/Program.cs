77a307e14ef28ff143758212fb86c3fbcd39e527
grxxxdy <mozjukhin@gmail.com>
2025-03-15 15:50:03 +0200

    Thread pool implementation

--- Parallel_Lab3/Program.cs
+++ Parallel_Lab3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ThreadPool threadPool = new ThreadPool(3, 2, 10);
+
+        for (int i = 0; i < 10; i++)
+        {
+            threadPool.EnqueueTask(WaitForRandomSeconds, i);
+        }
+    }
+
+    static void WaitForRandomSeconds()
+    {
+        Random rand = new Random();
+        
+        Thread.Sleep(rand.Next(6, 12) * 1000);
     }
 }
---
+++ Parallel_Lab3/ThreadPool.cs
@@ -0,0 +1,117 @@
+﻿namespace Parallel_Lab3;
+
+public class ThreadPool : IDisposable
+{
+    private List<Queue<(int, Action)>> _queues;
+    private List<Thread> _workers;
+    
+    private bool _shouldInterrupt;
+    private int _queueCapacity;
+    
+    private List<Mutex> _queueLocks;
+    private Mutex _consoleMutex;
+    
+    public ThreadPool(int queueAmount, int threadPerQueue, int queueCapacity)
+    {
+        _shouldInterrupt = false;
+        _queues = new List<Queue<(int, Action)>>();
+        _workers = new List<Thread>();
+        _queueCapacity = queueCapacity;
+        _queueLocks = new List<Mutex>();
+        _consoleMutex = new Mutex();
+        
+        for (int i = 0; i < queueAmount; i++)
+        {
+            // Initialize queue and it's mutex
+            _queues.Add(new Queue<(int, Action)>(queueCapacity));
+            
+            _queueLocks.Add(new Mutex());
+            
+            // Initialize threads for the queue
+            for (int j = 0; j < threadPerQueue; j++)
+            {
+                int threadId = i;
+                var worker = new Thread(() => ProcessTasks(threadId));
+                
+                _workers.Add(worker);
+                worker.Start();
+            }
+        }
+    }
+
+    public void EnqueueTask(Action task, int taskId)
+    {
+        Random rand = new Random();
+
+        int index = rand.Next(0, _queues.Count);        
+        
+        // Selected random queue, if it's full -> iterate through others
+        for (int i = 0; i < _queues.Count; i++)
+        {
+            index = (index + i) % _queues.Count;
+
+            _queueLocks[index].WaitOne();
+
+            if (_queues[index].Count < _queueCapacity)
+            {
+                _queues[index].Enqueue((taskId, task));
+                _queueLocks[index].ReleaseMutex();
+
+                _consoleMutex.WaitOne();
+                Console.WriteLine($"[Queue {index}] Task {taskId} has been enqueued.");
+                _consoleMutex.ReleaseMutex();
+                
+                return;
+            }
+            
+            _queueLocks[index].ReleaseMutex();
+        }
+        
+        _consoleMutex.WaitOne();
+        Console.WriteLine("All queues are full.");
+        _consoleMutex.ReleaseMutex();
+    }
+
+    private void ProcessTasks(int queueIndex)
+    {
+        while (!_shouldInterrupt)
+        {
+            _queueLocks[queueIndex].WaitOne();
+            
+            if(_queues[queueIndex].Count > 0)
+            {
+                var task = _queues[queueIndex].Dequeue();
+                _queueLocks[queueIndex].ReleaseMutex();
+                
+                _consoleMutex.WaitOne();
+                Console.WriteLine($"Task {task.Item1} has started.");
+                _consoleMutex.ReleaseMutex();
+                
+                task.Item2.Invoke();
+                
+                _consoleMutex.WaitOne();
+                Console.WriteLine($"Task {task.Item1} has completed.");
+                _consoleMutex.ReleaseMutex();
+            }
+            else
+            {
+                _queueLocks[queueIndex].ReleaseMutex();
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _shouldInterrupt = true;
+
+        foreach (var worker in _workers)
+        {
+            worker.Join();
+        }
+
+        foreach (var mutex in _queueLocks)
+        {
+            mutex.Dispose();
+        }
+    }
+}
